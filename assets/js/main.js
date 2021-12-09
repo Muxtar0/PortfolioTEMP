45b433737e5066c25474fb3196f88c391e403c8b
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2021-12-09 17:27:46 +0400

    scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,23 @@
-const hamburgerMenuIcon = document.querySelector('.hamburgerMenuIcon');
-hamburgerMenuIcon.addEventListener('click' , (e) => {
-    hamburgerMenuIcon.classList.toggle('active');
+const hamburgermenuicon = document.querySelector('.hamburgermenuicon');
+const openmenu = document.querySelector('.openmenu');
+hamburgermenuicon.addEventListener('click' , (e) => {
+    hamburgermenuicon.classList.toggle('active');
+    openmenu.classList.toggle('active');
+})
+
+/* template theme */
+const theme = document.querySelector('.theme');
+const body = document.querySelector('#body');
+const bannerbgimg = document.querySelector('#banner .bg-image img')
+theme.addEventListener('click' , (e) => {
+    if(e.target.classList.contains("lighttarget")){
+        body.classList.remove('dark');
+        body.classList.add('light');
+        bannerbgimg.src = 'assets/images/Frame 1.png';
+    }
+    if(e.target.classList.contains("darktarget")){
+        body.classList.remove('light');
+        body.classList.add('dark');
+        bannerbgimg.src = 'assets/images/FrameDark.png';
+    }
 })
