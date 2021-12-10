3ae2f42ccd06d9111f593d2108815058ea08d5ef
Muxtar0 <84911084+Muxtar0@users.noreply.github.com>
2021-12-10 22:08:04 +0400

    Theme changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,18 +23,21 @@ hamburgermenuicon.addEventListener('click' , (e) => {
 
 
 /* template theme */
-const theme = document.querySelector('.theme');
+const theme = document.querySelectorAll('.theme');
 const body = document.querySelector('#body');
 const bannerbgimg = document.querySelector('#banner .bg-image img')
-theme.addEventListener('click' , (e) => {
-    if(e.target.classList.contains("lighttarget")){
-        body.classList.remove('dark');
-        body.classList.add('light');
-        bannerbgimg.src = 'assets/images/Frame 1.png';
-    }
-    if(e.target.classList.contains("darktarget")){
-        body.classList.remove('light');
-        body.classList.add('dark');
-        bannerbgimg.src = 'assets/images/FrameDark.png';
-    }
-})
+for(let i = 0 ; i < theme.length ; i++){
+    theme[i].addEventListener('click' , (e) => {
+        if(e.target.classList.contains("lighttarget")){
+            body.classList.remove('dark');
+            body.classList.add('light');
+            bannerbgimg.src = 'assets/images/Frame 1.png';
+        }
+        if(e.target.classList.contains("darktarget")){
+            body.classList.remove('light');
+            body.classList.add('dark');
+            bannerbgimg.src = 'assets/images/FrameDark.png';
+        }
+    })
+}
+
